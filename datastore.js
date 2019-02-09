cc19cf02273954961d3f2bb12d617b75dd9cbd6b
john dang <jollyjohndang@gmail.com>
2019-02-08 23:21:32 -0800

    initial commit

---
+++ datastore.js
@@ -0,0 +1,58 @@
+questionArray = [
+  {
+  question: 'Who sponsored the 1992 Lithuanian Mens Basketball Team?',
+  answers: ['FIBA', 'Ross Perot', 'The Grateful Dead'],
+  correct: 'The Grateful Dead'
+},
+{
+  question: 'Which team did Lithuania defeat to win the Bronze medal?',
+  answers: ['Croatia', 'Unified Team (Former USSR), 'Team USA'],
+  correct: 'Unified Team (Former USSR)'
+},
+{
+question: 'What model car's trunk can fit 7'3" Arvydas Sabonis',
+answers: ['Cadillac', 'Lada', 'Volkswagen'],
+correct: 'Cadillac'
+},
+{
+  question:'The players used to purchase retail items like jeans and appliances
+  in Europe and the US to resell for a profit back home. Which player was 
+  considered the best 'entrepenuer'?',
+  answers: ['Valdemaras Chomicius', 'Arvydas Sabonis', 'Sarunas Marciulionis'],
+  correct: 'Valdemaras Chomicius'
+},
+{
+  question: 'Which documentary covered the team in the 1992 Olympics?',
+  answers: ['The Other Dream Team', 'The Dream Team', 'Space Jam'],
+  correct: 'The Other Dream Team'
+},
+{
+  question: 'Who played for the Portland Trailblazers from 1995-2003?',
+  answers: ['Sarunas Maciulionis', 'Rimas Kurtinaitis', 'Arvydas Sabonis'],
+  correct: 'Arvydas Sabonis'
+},
+{
+  question: 'During the 1989 San Francisco earthquake, this player appeared
+  at a commuter train accident in his Golden State Warriors warmup outfit 
+  pulling trapped passengers and administering first aid.',
+  answers: ['Valdemaras Chomicius', 'Arturus Karnisovas', 'Sarunas Marciulionis'],
+  correct: 'Sarunas Marciulionis'
+},
+{
+  question: 'Half of the team was born in this city, and the city itself
+  is home to BC Zalgiris, one of the oldest professional teams in the Euroleague.
+  What city is this?',
+  answers: ['Kaunas', 'Kretinga', 'Klaipeda'],
+  correct: 'Kaunas'
+},
+{
+  question: 'Rimas Kurtinaitis never played in the NBA, yet he did participate
+  in the NBA, in what event?',
+  answers: ['Coaching for the San Antonio Spurs', 'NBA All Star Weekend Dunk Contest', 'NBA All Star Weekend Three-Point Contest'],
+  correct: 'NBA All Star Weekend Three-Point Contest'
+},
+{
+  question: 'Who is Arvydas Sabonis' favorite basketball player?',
+  answers: ['Andrei Kirilenko', 'Shaquille O'neal', 'Larry Bird'],
+  correct: 'Larry Bird'
+}];
